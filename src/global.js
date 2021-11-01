6a1b0fb99f4cc01f99301467c9ea201023941026
Jean Meira <jean.meira10@hotmail.com>
2021-11-01 15:04:07 -0300

    Initial commit

---
+++ src/global.js
@@ -0,0 +1,18 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+html{
+  font-size: 62.5%;
+}
+`;
+
+export const Conteiner = styled.div`
+  width: 100%;
+  }
+`;
