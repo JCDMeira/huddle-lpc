ba798bbc29afead02cc22bba5be696ddccc12b6f
Jean Meira <jean.meira10@hotmail.com>
2021-11-03 14:26:12 -0300

    Add: conteiner and background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import { GlobalStyle } from './global';
+import { GlobalStyle, Conteiner } from './global';
+import desktopBackground from './assets/images/bg-hero-desktop.svg';
+import mobileBackground from './assets/images/bg-hero-mobile.svg';
 
 function App() {
+  const widthSize = window.screen.width;
   return (
     <>
       <GlobalStyle />
-      <h1>Hello world</h1>
+      <Conteiner widthSize={widthSize}>
+        <img
+          src={widthSize > 1023 ? desktopBackground : mobileBackground}
+          alt="background image"
+        />
+        <h1>Hello world</h1>
+      </Conteiner>
     </>
   );
 }
--- src/global.js
+++ src/global.js
@@ -14,5 +14,12 @@ html{
 
 export const Conteiner = styled.div`
   width: 100%;
+  height: 339.823rem;
+  img{
+    width: 100%;
+    position: absolute;
+    left: 0;
+    top: 0;
+  }
   }
 `;
